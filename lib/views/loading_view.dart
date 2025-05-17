c3ebe71a25f89e7ef7ca1452377601c9799c2ffe
kliuverson <kliverrui12@gmail.com>
2025-05-17 13:21:03 -0500

    Documentacion basica

--- lib/views/loading_view.dart
+++ lib/views/loading_view.dart
@@ -7,7 +7,14 @@ class Loading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [CircularProgressIndicator(), Text('Cargando...')],
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3N1bWU5aXpzY2Q2ZDk5NW1uYW5veGw5d2lyaDY2N2EyeWp2YzNyMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/i2tLw5ZyikSFdkeGHT/giphy.gif',
+            ),
+          ),
+          Text('Cargando...'),
+        ],
       ),
     );
   }
